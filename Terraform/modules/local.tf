ce68354f51beacccd4c5e7b2b2030574bfee6997
VishnuSharma11D00 <155800287+VishnuSharma11D00@users.noreply.github.com>
2024-05-19 20:19:16 +0530

    main, local variables files

---
+++ Terraform/modules/local.tf
@@ -0,0 +1,13 @@
+locals {
+  availability_zone1 = "us-east-1a"
+  availability_zone2 = "us-east-1b"
+
+  my_app_cluster_name = "my-app-cluster"
+  my_task_name = "my-task"
+  container_port = 3000
+  ecs_task_execution_role_name = "ecs_task_execution_role"
+  image_tag = "latest"
+  alb_name = "my-app-alb"
+  target_group_name = "my-app-tg"
+  my_app_service_name = "my-app-service"
+}
