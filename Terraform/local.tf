c49ceb9158f9b02958228f937fa10cf1e77b357c
VishnuSharma11D00 <155800287+VishnuSharma11D00@users.noreply.github.com>
2024-05-24 15:41:38 +0530

    Update local.tf
    Edit this local.tf file according to your project and aws account.
    
    important edits:
    codestar_connection_arn =
    AWS_ACCOUNT_ID =
    bucket_name =
    artifact_store_bucket_name =

--- Terraform/local.tf
+++ Terraform/local.tf
@@ -1,6 +1,10 @@
-locals {
-  bucket_name = "cc-tf-my-app-VS11d00"
+  env_region = "us-east-1"
+
+  bucket_name = "cc-tf-my-app-vs11d00"
   table_name = "ccTfDemo"
+
+  ecr_repo_name = "nodejs-server"
+  
   availability_zone1 = "us-east-1a"
   availability_zone2 = "us-east-1b"
 
@@ -12,4 +16,19 @@ locals {
   alb_name = "my-app-alb"
   target_group_name = "my-app-tg"
   my_app_service_name = "my-app-service"
+
+
+  codestar_connection_arn = "arn:aws:codestar-connections:..."
+  codestar_connection_name = "your codestar connection name"
+
+  artifact_store_bucket_name = "artifact-store-bucket-vs11d00"
+
+  AWS_ACCOUNT_ID = your account id (type -  number)
+  codebuild_project_name ="my_codebuild_project"
+  github_owner = "VishnuSharma11d00"
+  github_repo = "Nodejs-server"
+  github_repo_link = "https://github.com/VishnuSharma11D00/Nodejs-server.git"
+  codepipeline_name = "my_codepipeline"
+  github_branch = "main"
+  
 }
